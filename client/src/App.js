a0fb23115bebb99504eace6a1596ff91b84be649
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-03-27 13:17:15 -0500

    Load user & set token

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,15 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alerts from './components/layout/Alerts';
 
-
 import ContactState from './context/contact/ContactState';
 import AuthtState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
